9a80a6344a7b5e512c6d3285f262d85488c72200
Kim Wook Jin <151309651+ukjin9115@users.noreply.github.com>
2025-09-19 16:55:52 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -5,25 +5,18 @@ import java.io.*;
 
 public class Main {
     static int N;
-    static ArrayList<Integer>[] graph;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
-        long k = 1;
-        if(N != 0){
-            for(int i = N; i >= 1; i--) {
-                k *= i;
-            }
-            bw.write(String.valueOf(k));
-        }
-        else{
-            bw.write(String.valueOf(1));
-        }
+        bw.write(String.valueOf(recursive(N)));
+
         bw.close();
         br.close();
     }
-
-
+    public static long recursive(int n) {
+        if (n <= 1) return 1;
+        return n * recursive(n - 1);
+    }
 }
