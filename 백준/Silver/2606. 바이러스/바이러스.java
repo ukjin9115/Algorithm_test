6e90463009d71fa94dd51269d322ea826b18af77
Kim Wook Jin <151309651+ukjin9115@users.noreply.github.com>
2025-09-22 18:59:53 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static boolean[][] graph;
+    static boolean[] visited;
+    static int N,M;
+    static int answer;
+
+    public static void dfs(int idx){
+        visited[idx] = true;
+        answer++;
+
+        for(int i = 1; i <= N; i ++){
+            if(!visited[i] && graph[idx][i]){
+                dfs(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws  IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        graph = new boolean[N+1][N+1];
+        visited = new boolean[N+1];
+
+        int x,y;
+        for(int i = 0; i < M; i ++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            graph[x][y] = true;
+            graph[y][x] = true;
+         }
+        dfs(1);
+        //출력
+        bw.write(String.valueOf(answer-1));
+        br.close();
+        bw.close();
+
+    }
+}
