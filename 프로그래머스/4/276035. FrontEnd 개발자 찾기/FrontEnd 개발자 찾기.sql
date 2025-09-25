ca8062f9dfb3f0e13bc100c11319154f7c454b93
Kim Wook Jin <151309651+ukjin9115@users.noreply.github.com>
2025-09-25 15:50:14 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,20 @@
+-- 코드를 작성해주세요
+SELECT 
+    D.ID,
+    D.EMAIL,
+    D.FIRST_NAME,
+    D.LAST_NAME
+FROM DEVELOPERS D
+WHERE D.SKILL_CODE & 
+ (  SELECT SUM(CODE)
+    FROM SKILLCODES
+    WHERE CATEGORY = 'Front End') > 0
+ORDER BY D.ID ASC;
+
+
+
+    
+
+# (SELECT CODE
+# FROM SKILLCODES
+# WHERE CATEGORY = 'Front End')
